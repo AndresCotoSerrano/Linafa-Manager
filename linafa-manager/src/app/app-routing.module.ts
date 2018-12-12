d509daf04d283cbcbb503f77915d75a46d528bac
DiegoVgz <diego.ignacio.vega.rdz@gmail.com>
2018-12-12 17:59:22 -0600

    menus
    admin and managers pages

--- linafa-manager/src/app/app-routing.module.ts
+++ linafa-manager/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'menu-managers', loadChildren: './menu-managers/menu-managers.module#MenuManagersPageModule' },
+  { path: 'menu-admin', loadChildren: './menu-admin/menu-admin.module#MenuAdminPageModule' },
 ];
 
 @NgModule({
