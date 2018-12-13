6f939d68b4d7397369eae7b41fb50fff58f6e51f
Andres Coto <andres.coto3539@gmail.com>
2018-12-12 22:01:49 -0600

    a little bit change

--- linafa-manager/src/app/app-routing.module.ts
+++ linafa-manager/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginPageModule } from './login/login.module';
 
 const routes: Routes = [
-<<<<<<< HEAD
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'menu-managers', loadChildren: './menu-managers/menu-managers.module#MenuManagersPageModule' },
